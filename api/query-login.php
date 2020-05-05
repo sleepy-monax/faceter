49c994e2e92b3e114658af2641c241fbce2df8da
Kruril <misteurlova10@gmail.Com>
2020-05-05 22:16:53 +0200

    API: connection

---
+++ api/query-login.php
@@ -0,0 +1,20 @@
+<?php
+
+$connection = include 'connection.php';
+
+$login = mysqli_real_escape_string($connection,strval($_GET["userName"]));
+$field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'userMail': 'userName';
+
+$password = mysqli_real_escape_string($connection,strval($_GET["password"]));
+
+$requestLogin = 'select * from User where '. $field.'= \''. $login . '\' and userPassword= \''
+    . $password .'\'';
+$result = mysqli_query($connection, $requestLogin);
+
+if ($row = mysqli_fetch_assoc($result)) {
+    print json_encode($row['userId']);
+}
+else {
+    print json_encode(false);
+}
+
