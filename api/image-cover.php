33860ce1e662b5085ccbe8de679e2a015ca3a5a0
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-05-06 19:29:03 +0200

    API: Fixed users and covers naming.

--- api/image-cover.php
+++ api/image-cover.php
@@ -1,8 +1,8 @@
 <?php
 
-$imgProfile = "../res/cover/" . strval($_GET["profileId"]) . ".jpg";
+$imgProfile = "../res/covers/" . strval($_GET["profileId"]) . ".jpg";
 
 if (file_exists($imgProfile))
-    print json_encode("/res/cover/" . strval($_GET["profileId"]) . ".jpg");
+    print json_encode("/res/covers/" . strval($_GET["profileId"]) . ".jpg");
 else
-    print json_encode("/res/cover/default-cover.jpg");
+    print json_encode("/res/covers/default.jpg");
--- api/image-profile.php
+++ api/image-profile.php
@@ -1,8 +1,8 @@
 <?php
 
-$imgProfile = "../res/user/" . strval($_GET["profileId"]) . ".jpg";
+$imgProfile = "../res/users/" . strval($_GET["profileId"]) . ".jpg";
 
 if (file_exists($imgProfile))
-    print json_encode("/res/user/" . strval($_GET["profileId"]) . ".jpg");
+    print json_encode("/res/users/" . strval($_GET["profileId"]) . ".jpg");
 else
-    print json_encode("/res/user/default-profile.jpg");
+    print json_encode("/res/users/default.jpg");
