0957aa42b5c150637db3835de894ea2195b448bb
Kruril <misteurlova10@gmail.Com>
2020-05-07 15:55:41 +0200

    Components : CreatePost

---
+++ app/components/CreatePost.js
@@ -0,0 +1,72 @@
+import { Component } from '/lib/preact.js';
+import Icon from '/app/components/Icon.js';
+
+
+class CreatePost extends Component {
+    state = {
+        content: '',
+    }
+
+    styleCreatePost = {
+        display: "flex",
+        backgroundColor: "var(--theme-middleground)",
+        height: "32px",
+        marginBottom: "8px",
+        marginTop: "8px",
+        borderRadius: "8px",
+        paddingLeft: "8px",
+        overflow: "hidden"
+    }
+
+    styleField = {
+        backgroundColor: "transparent",
+        color: "var(--theme-foreground)",
+        flexGrow: "1",
+        width: "100%",
+        outline: "none",
+        border: "none"
+    }
+
+    styleButton = {
+        width: "64px",
+        textAlign: "center",
+        verticalAlign: "middle",
+        lineHeight: "32px",
+        userSelect: "none",
+        ':hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        }
+    }
+
+    constructor() {
+        super();
+    }
+
+    onTextInput = e => {
+        let content = e.target.value;
+        this.setState({ content });
+
+        if (this.props.onInput)
+        {
+            this.props.onInput(content);
+        }
+    }
+
+    render() {
+        return html`
+<span style=${this.styleCreatePost}>
+    <input
+        type="text"
+        style=${this.styleField}
+        placeholder="Quoi de neuf ?"
+        value=${this.state.content}
+        onInput=${this.onTextInput}
+    />
+    <span style=${this.styleButton} class='overable'>
+        <${Icon} icon="send"/>
+    </span>
+</span>`;
+    }
+}
+
+export { CreatePost as default };
--- app/routes/Profile.js
+++ app/routes/Profile.js
@@ -1,11 +1,43 @@
-import { Component } from '/lib/preact.js';
+import {Component} from '/lib/preact.js';
 import Post from '/app/components/Post.js';
-import { getUser } from '/app/model/Users.js';
+import {getUser} from '/app/model/Users.js';
+import CreatePost from '/app/components/CreatePost.js';
 
 class Profile extends Component {
     state = {
         user: undefined,
         posts: []
+    }
+    center = {
+        textAlign: 'center'
+    }
+
+    containerProfile = {
+        position: 'relative'
+    }
+
+    coverImage = {
+        height: '320px',
+        width: '100%',
+        objectFit: 'cover',
+    }
+
+    profileImage = {
+        position: 'absolute',
+        borderRadius: '192px',
+        width: '192px',
+        height: '192px',
+
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        left: '0',
+        right: '0',
+        top: '0',
+        bottom: '0',
+
+        boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
     }
 
     constructor() {
@@ -15,28 +47,27 @@ class Profile extends Component {
     componentDidMount() {
 
         fetch("/api/query-posts-user.php?postAuthor=" + this.props.profileId)
-            .then(function (response) { return response.json()})
+            .then(function (response) {
+                return response.json()
+            })
             .then(posts => {
-                this.setState({ posts });
+                this.setState({posts});
             });
 
-        getUser(this.props.profileId, user => this.setState({ user }));
+        getUser(this.props.profileId, user => this.setState({user}));
     }
 
     render() {
         return html`
-        <div class="container-profile">
-            <img class="cover-image" src="${this.state.user ? this.state.user.coverPic : '/res/covers/default.jpg'}" />
-            <img class="profile-image ahead" src="${this.state.user ? this.state.user.profilePic : '/res/users/default.jpg'}"/>
+        <div style=${this.containerProfile}>
+            <img style=${this.coverImage} src="${this.state.user ? this.state.user.coverPic : '/res/covers/default.jpg'}" />
+            <img style=${this.profileImage} src="${this.state.user ? this.state.user.profilePic : '/res/users/default.jpg'}"/>
         </div>
-        <div class="container">
-            <div class="new-post">
-                <input type="text" placeholder="Quoi de neuf ? " class="bg-transparent border-0"/>
-                <input type="button" class="bg-transparent border-0 button" value="send"/>
-            </div>
+        <div style=${this.newPost} class="container">
+            <${CreatePost}/>
             ${this.state.posts.map(post => html`<${Post} postId="${post.postId}"/>`)}
         </div>`;
     }
 }
 
-export { Profile as default };
+export {Profile as default};
