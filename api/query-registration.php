0e9227b4292a29096600fe7b2520517296985303
AiIurus <mathieu.trempont@hotmail.com>
2020-05-08 13:32:11 +0200

    app/routes : Modif visuelle inscription

--- api/query-registration.php
+++ api/query-registration.php
@@ -6,14 +6,27 @@ $username = mysqli_real_escape_string($connection,strval($_GET["username"]));
 $password = mysqli_real_escape_string($connection,strval($_GET["password"]));
 $email = mysqli_real_escape_string($connection,strval($_GET["email"]));
 
+// VERIFICATION DU PSEUDO
 $verifPseudo  = 'SELECT COUNT(*) AS nbr FROM User WHERE userName = \'' . $username . '\'';
-    $res  = mysqli_query($connection, $verifPseudo);
-    $alors  = mysqli_fetch_assoc($res);
+    $resPseudo  = mysqli_query($connection, $verifPseudo);
+    $alorsPseudo  = mysqli_fetch_assoc($resPseudo);
          
 // BOUCLE POUR INFORMER L'UTLISATEUR
   if(isset($username)){
-    if(!($alors['nbr'] == 0)){
-      echo "Ce pseudo est déjà utilisé !";
+    if(!($alorsPseudo['nbr'] == 0)){
+      //echo "Ce pseudo est déjà utilisé !";
+    } 
+  }
+
+// VERIFICATION DE L'ADRESSE MAIL
+$verifEmail  = 'SELECT COUNT(*) AS nbr FROM User WHERE userMail = \'' . $email . '\'';
+    $resEmail  = mysqli_query($connection, $verifEmail);
+    $alorsEmail  = mysqli_fetch_assoc($resEmail);
+         
+// BOUCLE POUR INFORMER L'UTLISATEUR
+  if(isset($email)){
+    if(!($alorsEmail['nbr'] == 0)){
+      //echo "Cette adresse mail est déjà utilisée !";
     }
   }
 
@@ -23,22 +36,14 @@ where not exists (SELECT * FROM User
                     WHERE userName = \'' . $username . '\'
                     OR userMAIL = \'' . $email . '\')';
 
-$result = mysqli_query($connection, $sql);\
+$result = mysqli_query($connection, $sql);
 
-print_r($_POST);
-print $sql;
-
-/*$rows = array();
-while($r = mysqli_fetch_assoc($result)) {
-    $rows[] = $r;
+if($r = mysqli_fetch_assoc($result)) {
+    print json_encode($r);
 }
-//print json_encode($rows);
-if ($rows = mysqli_fetch_assoc($result)) {
-    print json_encode($rows);
-}
-else {
+else
+{
     print json_encode(false);
-}*/
-
+}
 
 ?>
--- app/routes/Registration.js
+++ app/routes/Registration.js
@@ -1,5 +1,6 @@
 import { Component } from '/lib/preact.js';
 import { route } from '/lib/preact-router.js';
+import Icon from '/app/components/Icon.js';
 
 class Registration extends Component {
     state = {
@@ -12,6 +13,72 @@ class Registration extends Component {
         super();
     }
 
+    styleTitle = {
+        textAlign: 'center'
+    }
+
+    styleAlert = {
+        float: "left",
+        marginRight: ".3em",
+    }
+
+    styleError = {
+        padding: ".7em .7em"
+    }
+
+    styleHide = {
+        visibility: "hidden",
+        display: "none"
+    }
+
+    styleVisible = {
+        visibility: 'visible',
+        display: 'block',
+    }
+
+    styleConnection = {
+        backgroundColor: 'var(--theme-background-alt)',
+        color: 'var(--theme-foreground)',
+        padding: '16px 16px',
+        marginBottom: '16px',
+        borderRadius: '8px',
+    }
+
+    styleInput = {
+        backgroundColor: "transparent",
+        color: "var(--theme-foreground)",
+        flexGrow: "1",
+        width: "100%",
+        outline: "none",
+        border: "none"
+    }
+
+    styleField = {
+        display: "flex",
+        backgroundColor: "var(--theme-middleground)",
+        height: "32px",
+        marginBottom: "8px",
+        marginTop: "8px",
+        borderRadius: "8px",
+        paddingLeft: "8px",
+        overflow: "hidden"
+    }
+
+    styleIcon = {
+        width: "64px",
+        textAlign: "center",
+        verticalAlign: "middle",
+        lineHeight: "32px",
+        userSelect: "none",
+        ':hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.1)'
+        }
+    }
+
+    styleButton = {
+        width: "49%"
+    }
+
     doBack = e => {
         route("/login");
     }
@@ -19,14 +86,12 @@ class Registration extends Component {
     doRegistration = e => {
         fetch("/api/query-registration.php?username=" + this.state.username + "&password=" + this.state.password + "&email=" + this.state.email)
         .then(function (response) { return response.json()})
-        /*.then(registration => {
-            if (registration != false) {
-                alert("Félicitations, vous êtes maintenant inscrit");
-                route("/login");
-            }
-            else
-                alert("Identifiant ou adresse mail déja utilisés")
-        })*/
+        .then(registration => {
+            if (registration != false)
+                route("/login")
+            else 
+                document.getElementById('alertInfo').style = this.styleVisible;
+        });
     }
 
     onSubmit = e => {
@@ -50,24 +115,60 @@ class Registration extends Component {
 
     render() {
         return html`
-        <div>
-            <form onSubmit=${this.onSubmit}>
-                <h1>Inscription</h1>
-                <div>
-                    Pseudo :
-                    <input type="text" id="username" name="username" value=${this.state.username} onInput=${this.onUsernameInput} />
+        <div class="container">
+            <form style=${this.styleConnection} onSubmit=${this.onSubmit}>
+                <h1 style=${this.styleTitle}>Inscription</h1>
+                <div class="ui-widget" style=${this.styleHide} id="alertInfo">
+                    <div class="ui-state-error ui-corner-all" style=${this.styleError}>
+                        <p>
+                            <span class="ui-icon ui-icon-alert" style=${this.styleAlert}></span>
+                            <strong>Alert : </strong>
+                            "Pseudo ou adresse mail déja utilisés"
+                        </p>
+                    </div>
+                </div>
+                <div style=${this.styleField}>
+                    <span style=${this.styleIcon}>
+                        <${Icon} icon="perm_identity" />
+                    </span>
+                    <input 
+                        type="text" 
+                        value=${this.state.username} 
+                        onInput=${this.onUsernameInput}
+                        style=${this.styleInput} 
+                        placeholder="Pseudo"
+                        required
+                    />
+                </div>
+                <div style=${this.styleField}>
+                    <span style=${this.styleIcon}>
+                        <${Icon} icon="lock_open" />
+                    </span>
+                    <input
+                        type="text" 
+                        value=${this.state.password} 
+                        onInput=${this.onPasswordInput} 
+                        style=${this.styleInput} 
+                        placeholder="Mot de passe"
+                        required
+                    />
+                </div>
+                <div style=${this.styleField}>
+                    <span style=${this.styleIcon}>
+                        <${Icon} icon="email" />
+                    </span>
+                    <input 
+                        type="text" 
+                        value=${this.state.email} 
+                        onInput=${this.onEmailInput} 
+                        style=${this.styleInput} 
+                        placeholder="Adresse mail   "
+                        required
+                    />
                 </div>
                 <div>
-                    Mot de passe :
-                    <input type="password" id="password" name="password" value=${this.state.password} onInput=${this.onPasswordInput} />
-                </div>
-                <div>
-                    Adresse mail :
-                    <input type="text" id="email" name="email" value=${this.state.email} onInput=${this.onEmailInput} />
-                </div>
-                <div>
-                    <button onClick=${this.doRegistration}>S’inscrire</button>
-                    <button onClick=${this.doBack}>Retour</button>
+                    <button class="ui-button" style=${this.styleButton} onClick=${this.doRegistration}>S’inscrire</button>
+                    <button class="ui-button" style=${this.styleButton} onClick=${this.doBack}>Retour</button>
                 </div>
             </form>
         </div>`;
