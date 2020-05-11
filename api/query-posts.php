9a5155c8abe71510fc50a1db9b2f5511668375bd
Kruril <misteurlova10@gmail.Com>
2020-05-11 09:14:37 +0200

    API : query-posts return only followed's post

--- api/query-posts.php
+++ api/query-posts.php
@@ -1,7 +1,14 @@
 <?php
 $connection = include 'connection.php';
 
-$sql = 'select postId from Post order by postDate desc';
+$sql = 'select postId
+        from Post
+        where postAuthor in (select us2.userId as idUser
+                             from follow
+                                    inner join user as us1 on follow.followerId = us1.userId
+                                    inner join user as us2 on follow.followedId = us2.userId
+                             where us1.userId = 2)
+        order by postDate desc';
 $result = mysqli_query($connection, $sql);
 
 $rows = array();
@@ -9,4 +16,3 @@ while($r = mysqli_fetch_assoc($result)) {
     $rows[] = $r;
 }
 print json_encode($rows);
-?>
